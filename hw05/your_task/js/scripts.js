28f46923dedc8e4f12f4e5b7b5311cd7dc54ffd0
bsocarras <brandos759@gmail.com>
2020-02-24 23:34:38 -0600

    1 and 2 but I can't do url's correctly

--- hw05/your_task/js/scripts.js
+++ hw05/your_task/js/scripts.js
@@ -29,7 +29,45 @@ const getAlbums = term => {
         get albums from spotify based on the search term
         "${term}" and load them into the #albums section 
         of the DOM...`);
+      fetch(baseURL+ "?type=tracks&q=" + term)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          if(data.length == 0) {
+            document.querySelector('#tracks').innerHTML = `
+              <section class="artist-card">
+                <a>
+                  Artist not in Spotify database. No tracks found. 
+                <a>
+              </section>
+            `;
+          }
+          
+          document.querySelector('#tracks').innerHTML = '';
+          for (i = 0; i < 5; i++) {
+            // if (data[i].length = 0) break;
+
+            document.querySelector('#tracks').innerHTML += getTracksHTML(data[i]);
+          }
+    });
+
 };
+
+const getTracksHTML = (tData) => {
+  const template = `
+    <section class="track-item preview" data-preview-track="${tData.preview_url}">
+    <img src="${tData.album.image_url}">
+    <i class="fas play-track fa-play" aria-hidden="true"></i>
+    <div class="label">
+        <h3>"${tData.name}"</h3>
+        <p>
+          "${tData.artist.name}"
+        </p>
+    </div>
+  </section>
+  `;
+  return template
+}
 
 const getArtist = term => {
   console.log(`
@@ -37,11 +75,35 @@ const getArtist = term => {
         "${term}" and load the first artist into the #artist section 
         of the DOM...`);
 
-  response = fetch(baseURL + "?type=artist&q=" + term)
+  fetch(baseURL + "?type=artist&q=" + term)
     .then(response => response.json())
     .then(data => {
-      console.log(data).then(data[0]);
+      console.log(data);
+      if(data.length == 0) {
+        document.querySelector('#artist').innerHTML = `
+          <section class="artist-card">
+            <a>
+              Artist not in Spotify database. 
+            <a>
+          </section>
+        `;
+      }
+
+      document.querySelector('#artist').innerHTML = `
+        <section class="artist-card" id=${data[0].id}>
+          <div>
+              <img src="${data[0].img_url}">
+              <h3>${data[0].name}</h3>
+              <div class="footer">
+                  <a href=${data[0].spotify_url} target="_blank">
+                      view on spotify
+                  </a>
+              </div>
+          </div>
+      </section>
+      `;
     });
+
 };
 
 document.querySelector("#search").onkeyup = ev => {
